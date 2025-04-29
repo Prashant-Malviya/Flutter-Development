98fc4ff57bf7db298dd7d7c6d6e3d7488daa9ad1
Prashant Malviya <prashantmalviya272002@gmail.com>
2025-04-29 14:33:13 +0530

    Center Widget done

---
+++ StudioProjects/Widgets/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+
+          width: 200,
+          height: 200,
+          color: Colors.orange,
+
+          child: Center(child: Text("Namaste Bharat", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
+        ),
+      ),
+    );
+  }
+}
