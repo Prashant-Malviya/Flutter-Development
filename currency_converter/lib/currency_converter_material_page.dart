75cf8232f408e8e0c8d7fec637c8895d4c28b324
Shravan Jha <shravan.jha@gmail.com>
2025-05-27 19:47:24 +0530

    flutter code updated R

---
+++ currency_converter/lib/currency_converter_material_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+
+const CurrencyConverterMaterialPage({super.key});
+
+
+@override
+
+Widget build(BuildContext context){
+
+  return const Scaffold(
+    body: Center(child: Text('Namaste India'),
+    ),
+  );
+}
+
+}
