b0c61ce7c0692c0bd06dde6d57811e00f6098563
Prashant-Malviya <prashantmalviya272002@gmail.com>
2025-06-22 20:43:22 +0530

    code updated

--- currency_converter/lib/main.dart
+++ currency_converter/lib/main.dart
@@ -10,18 +10,14 @@ void main() {
 //2. Statefulwidgets : state change
 //3. InheritedWidgets
 
-
 //1. Material Design
 
 //2. Cupertino Design
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CurrencyConverterMaterialPage(),
-    );
+    return const MaterialApp(home: CurrencyConverterMaterialPage());
   }
 }
